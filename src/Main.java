8fe010cc15adb41ab0351377a7c59588e2244156
VasyukovEvgeny <e.vasyukov@i-teco.ru>
2022-02-18 07:08:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main (String[] args) {
+
+        System.out.println("___________________'\"_____");
+        System.out.println("__________________---_____");
+        System.out.println("______________/\\_|_|_____");
+        System.out.println("_____________/__\\|_|____");
+        System.out.println("____________/____\\_|____");
+        System.out.println("___________/______\\|____");
+        System.out.println("__________/__[___]_\\____");
+        System.out.println("_________/|________|\\____");
+        System.out.println("________/_|________|_\\____");
+        System.out.println("__________|__---___|_______");
+        System.out.println("__________|__| |___|_______");
+
+    }
+}
